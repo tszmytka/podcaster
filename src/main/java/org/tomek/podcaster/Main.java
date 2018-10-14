77c60818b0047abef6fc9e520dcfde1051335094
Tomek <tszmytka@gmail.com>
2018-10-14 21:55:51 +0200

    Implement finding the podcast url

--- src/main/java/org/tomek/podcaster/Main.java
+++ src/main/java/org/tomek/podcaster/Main.java
@@ -1,10 +1,12 @@
 package org.tomek.podcaster;
 
+import org.tomek.podcaster.finder.TokFmFinder;
 import org.tomek.podcaster.runner.ExternalAppRunner;
 
 public class Main {
     public static void main(String[] args) {
-        ExternalAppRunner runner = new ExternalAppRunner("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", "http://lodz.radio.pionier.net.pl:8000/pl/tuba10-1.mp3");
+        TokFmFinder finder = new TokFmFinder("67938");
+        ExternalAppRunner runner = new ExternalAppRunner("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", finder.findPodcastUrl());
         runner.run();
     }
 }
---
+++ src/main/java/org/tomek/podcaster/finder/TokFmFinder.java
@@ -0,0 +1,47 @@
+package org.tomek.podcaster.finder;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TokFmFinder {
+    private final static String SOURCE_URL = "https://audycje.tokfm.pl/gets";
+    private final static Gson GSON = new Gson();
+
+    private final String podcastId;
+
+    public TokFmFinder(String podcastId) {
+        this.podcastId = podcastId;
+    }
+
+
+    public String findPodcastUrl() {
+        try {
+            URL url = new URL(SOURCE_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                HashMap<String, String> inParams = new HashMap<>();
+                inParams.put("st", "tokfm");
+                inParams.put("pid", podcastId);
+                outputStream.writeBytes(GSON.toJson(inParams));
+            }
+
+            try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                Map outParams = GSON.fromJson(inputReader.readLine(), Map.class);
+                return (String) outParams.get("url");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+}
