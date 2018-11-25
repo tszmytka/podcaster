11038fcf1a9add5591f44976aa0385d1333e8dba
Tomek <tszmytka@gmail.com>
2018-11-25 18:15:32 +0100

    Add dependencies for mockito and hamcrest. Extend tests for category provider

--- src/main/java/org/tomek/podcaster/config/providers/TokFmConfig.java
+++ src/main/java/org/tomek/podcaster/config/providers/TokFmConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.tomek.podcaster.parser.jsoup.JsoupConnector;
 import org.tomek.podcaster.tokfm.CategoryProvider;
 import org.tomek.podcaster.tokfm.PodcastProvider;
 
@@ -16,9 +17,22 @@ import java.net.URL;
 public class TokFmConfig {
     private String categoriesUrl;
 
+    private final JsoupConnector jsoupConnector;
+
+
+    public TokFmConfig(JsoupConnector jsoupConnector) {
+        this.jsoupConnector = jsoupConnector;
+    }
+
+    @Bean
+    public JsoupConnector jsoupConnector() {
+        return new JsoupConnector();
+    }
+
+
     @Bean
     public CategoryProvider categoryProvider() throws MalformedURLException {
-        return new CategoryProvider(new URL(categoriesUrl));
+        return new CategoryProvider(jsoupConnector, new URL(categoriesUrl));
     }
 
 
