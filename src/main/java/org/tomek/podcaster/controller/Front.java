0de388ec04e6c574204e3a358d84404944e7eabf
Tomek <tszmytka@gmail.com>
2018-12-09 14:47:39 +0100

    Prepare main menu with 2 actions

--- src/main/java/org/tomek/podcaster/controller/Front.java
+++ src/main/java/org/tomek/podcaster/controller/Front.java
@@ -1,12 +1,9 @@
 package org.tomek.podcaster.controller;
 
-import javafx.geometry.Insets;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -22,9 +19,9 @@ public class Front {
     private final CategoryProvider categoryProvider;
     private final PodcastProvider podcastProvider;
 
-    private ListView<Category> lvCategories = new ListView<>();
-    private ListView<Podcast> lvPodcasts = new ListView<>();
-    private Button buttonPlay = new Button("Play");;
+    private ListView<Category> lvCategories;
+    private ListView<Podcast> lvPodcasts;
+    private Button buttonPlay;
 
 
     public Front(CategoryProvider categoryProvider, PodcastProvider podcastProvider) {
@@ -34,64 +31,96 @@ public class Front {
 
 
     public void render(Stage primaryStage) {
-        Label label = new Label("Select from the categories and podcasts below.");
-
-        lvCategories.getItems().addAll(categoryProvider.getCategories().values());
-        lvCategories.setCellFactory(param -> new ListCell<Category>() {
-            @Override
-            protected void updateItem(Category category, boolean empty) {
-                super.updateItem(category, empty);
-                setText(empty ? null : category.getName());
-            }
-        });
-        lvCategories.setMinWidth(300);
-        lvCategories.setOnMouseClicked(event -> {
-            Category category = lvCategories.getSelectionModel().getSelectedItem();
-            try {
-                lvPodcasts.setDisable(false);
-                buttonPlay.setDisable(true);
-                lvPodcasts.getItems().setAll(podcastProvider.getPodcasts(new URL(category.getUrl())).values());
-            } catch (MalformedURLException e) {
-                //
-            }
-        });
-
-        lvPodcasts.setDisable(true);
-        lvPodcasts.setMinWidth(450);
-        lvPodcasts.setCellFactory(param -> new ListCell<Podcast>() {
-            @Override
-            protected void updateItem(Podcast podcast, boolean empty) {
-                super.updateItem(podcast, empty);
-                setText(empty ? null : podcast.getTitle());
-            }
-        });
-        lvPodcasts.setOnMouseClicked(event -> {
-            buttonPlay.setDisable(false);
-        });
-
-        buttonPlay.setDisable(true);
-        buttonPlay.setOnMouseClicked(event -> {
-            Podcast podcast = lvPodcasts.getSelectionModel().getSelectedItem();
-
-        });
-
         BorderPane borderPane = new BorderPane();
-        HBox topBox = new HBox(20, label);
-        topBox.setPadding(new Insets(5));
-        borderPane.setTop(topBox);
+        borderPane.setTop(buildMenuBar());
+        borderPane.setLeft(new HBox(getLvCategories()));
+        borderPane.setCenter(new HBox(getLvPodcasts()));
 
-        HBox leftBox = new HBox(lvCategories);
-        borderPane.setLeft(leftBox);
-
-        HBox centerBox = new HBox(lvPodcasts);
-        borderPane.setCenter(centerBox);
-
-        HBox bottomBox = new HBox(5, buttonPlay);
+        HBox bottomBox = new HBox(5, getButtonPlay());
         bottomBox.setAlignment(Pos.BOTTOM_RIGHT);
         borderPane.setBottom(bottomBox);
 
         primaryStage.setTitle("Podcaster");
         primaryStage.setScene(new Scene(borderPane, 800, 600));
         primaryStage.show();
+    }
+
+    private ListView<Category> getLvCategories() {
+        if (lvCategories == null) {
+            lvCategories = new ListView<>();
+            lvCategories.getItems().addAll(categoryProvider.getCategories().values());
+            lvCategories.setCellFactory(param -> new ListCell<Category>() {
+                @Override
+                protected void updateItem(Category category, boolean empty) {
+                    super.updateItem(category, empty);
+                    setText(empty ? null : category.getName());
+                }
+            });
+            lvCategories.setMinWidth(300);
+            lvCategories.setOnMouseClicked(event -> {
+                Category category = lvCategories.getSelectionModel().getSelectedItem();
+                try {
+                    ListView<Podcast> lvPodcasts = getLvPodcasts();
+                    lvPodcasts.setDisable(false);
+                    getButtonPlay().setDisable(true);
+                    lvPodcasts.getItems().setAll(podcastProvider.getPodcasts(new URL(category.getUrl())).values());
+                } catch (MalformedURLException e) {
+                    //
+                }
+            });
+        }
+        return lvCategories;
+    }
+
+    private ListView<Podcast> getLvPodcasts() {
+        if (lvPodcasts == null) {
+            lvPodcasts = new ListView<>();
+            lvPodcasts.setDisable(true);
+            lvPodcasts.setMinWidth(450);
+            lvPodcasts.setCellFactory(param -> new ListCell<Podcast>() {
+                @Override
+                protected void updateItem(Podcast podcast, boolean empty) {
+                    super.updateItem(podcast, empty);
+                    setText(empty ? null : podcast.getTitle());
+                }
+            });
+            lvPodcasts.setOnMouseClicked(event -> {
+                buttonPlay.setDisable(false);
+            });
+        }
+        return lvPodcasts;
+    }
+
+    private Button getButtonPlay() {
+        if (buttonPlay == null) {
+            buttonPlay = new Button("Play");
+            buttonPlay.setDisable(true);
+            buttonPlay.setOnMouseClicked(event -> {
+                Podcast podcast = getLvPodcasts().getSelectionModel().getSelectedItem();
+            });
+        }
+        return buttonPlay;
+    }
+
+    private MenuBar buildMenuBar() {
+        Menu menuFile = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(actionEvent -> {
+            Platform.exit();
+        });
+        menuFile.getItems().add(exit);
+
+        Menu menuHelp = new Menu("Help");
+        MenuItem about = new MenuItem("About");
+        about.setOnAction(e -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("About");
+            alert.setHeaderText(null);
+            alert.setContentText("Podcaster ver 0.3");
+            alert.show();
+        });
+
+        menuHelp.getItems().add(about);
+        return new MenuBar(menuFile, menuHelp);
     }
 }
