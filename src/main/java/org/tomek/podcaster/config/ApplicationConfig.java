a8b854c2ccaa4fd8cbe620135ad306ff504a1626
Tomek <tszmytka@gmail.com>
2018-12-10 22:18:30 +0100

    Provide media player path through configuration

--- src/main/java/org/tomek/podcaster/config/ApplicationConfig.java
+++ src/main/java/org/tomek/podcaster/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package org.tomek.podcaster.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.tomek.podcaster.controller.Front;
@@ -13,23 +14,29 @@ public class ApplicationConfig {
     private final CategoryProvider categoryProvider;
     private final PodcastProvider podcastProvider;
     private final PodcastUrlProvider podcastUrlProvider;
+    private final String mediaPlayerPath;
 
 
-    public ApplicationConfig(CategoryProvider categoryProvider, PodcastProvider podcastProvider, PodcastUrlProvider podcastUrlProvider) {
+    public ApplicationConfig(
+        CategoryProvider categoryProvider,
+        PodcastProvider podcastProvider,
+        PodcastUrlProvider podcastUrlProvider,
+        @Value("${player.path}") String mediaPlayerPath
+    ) {
         this.categoryProvider = categoryProvider;
         this.podcastProvider = podcastProvider;
         this.podcastUrlProvider = podcastUrlProvider;
+        this.mediaPlayerPath = mediaPlayerPath;
     }
-
 
     @Bean
     public Front front(PodcastPlayerRunner podcastPlayerRunner) {
         return new Front(categoryProvider, podcastProvider, podcastUrlProvider, podcastPlayerRunner);
     }
 
-
     @Bean
     public PodcastPlayerRunner podcastPlayerRunner() {
-        return new PodcastPlayerRunner("C:\\Program Files\\VideoLAN\\VLC\\vlc.exe");
+        return new PodcastPlayerRunner(mediaPlayerPath);
     }
+
 }
