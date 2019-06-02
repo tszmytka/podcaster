e7d3cb68680bf72123562fefaf72737955dc1bcd
Tomek <tszmytka@gmail.com>
2019-06-02 17:01:26 +0200

    Break jdk8 backwards compatibility. Use immutable collections

--- src/test/java/dev/tomek/podcaster/duration/DurationFormatterTest.java
+++ src/test/java/dev/tomek/podcaster/duration/DurationFormatterTest.java
@@ -3,6 +3,7 @@ package dev.tomek.podcaster.duration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +11,11 @@ class DurationFormatterTest {
     @Test
     void canFormatCorrectly() {
         DurationFormatter df = new DurationFormatter("%d", "%d", "%d", ":");
-        assertEquals("0:45", df.format(Duration.ofSeconds(45)));
+        Map<String, Duration> inputData = Map.of(
+            "0:45", Duration.ofSeconds(45)
+        );
+        for (Map.Entry<String, Duration> entry : inputData.entrySet()) {
+            assertEquals(entry.getKey(), df.format(entry.getValue()));
+        }
     }
 }
